c27f5e876c4ad28e5c8c086bef3705a591221841
ivangufler <ivan.gufler@tfobz.net>
2020-03-24 03:25:58 +0100

    changed name of the function to printc (print color)

--- colors.h
+++ colors.h
@@ -21,23 +21,23 @@
     static const int white_f = 97;
 
     // Background colors
-    static const int default_b = 39;
-    static const int black_b = 30;
-    static const int red_b = 31;
-    static const int green_b = 32;
-    static const int yellow_b = 33;
-    static const int blue_b = 34;
-    static const int magenta_b = 35;
-    static const int cyan_b = 36;
-    static const int lightgray_b = 37;
-    static const int darkgray_b = 90;
-    static const int lightred_b = 91;
-    static const int lightgreen_b = 92;
-    static const int lightyellow_b = 93;
-    static const int lightblue_b = 94;
-    static const int lightmagenta_b = 95;
-    static const int lightcyan_b = 96;
-    static const int white_b = 97;
+    static const int default_b = 49;
+    static const int black_b = 40;
+    static const int red_b = 41;
+    static const int green_b = 42;
+    static const int yellow_b = 43;
+    static const int blue_b = 44;
+    static const int magenta_b = 45;
+    static const int cyan_b = 46;
+    static const int lightgray_b = 47;
+    static const int darkgray_b = 100;
+    static const int lightred_b = 101;
+    static const int lightgreen_b = 102;
+    static const int lightyellow_b = 103;
+    static const int lightblue_b = 104;
+    static const int lightmagenta_b = 105;
+    static const int lightcyan_b = 106;
+    static const int white_b = 107;
 
     // text sets
     static const int bold = 1;
@@ -47,6 +47,6 @@
     static const int reverse = 7;
     static const int hidden = 8;
 
-    void color(char *text, int set, int color, int background);
+    void printc(char *text, int set, int color, int background);
 
     #endif //CHATROOM_COLORS_H
